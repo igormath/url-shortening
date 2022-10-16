2c92f7f08f4286e913ddf38d5b0b8a4abc4cc87f
igormath <igorxt9@gmail.com>
2022-10-16 10:49:36 -0300

    :art: Finish media querie for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,17 @@ const Header = () => {
     return (
         <header>
             <h1 className="header__title">Shortly</h1>
+            <nav className="header__menu">
+                <ul className="header__menu__options">
+                    <li><a href="">Features</a></li>
+                    <li><a href="">Pricing</a></li>
+                    <li><a href="">Resources</a></li>
+                </ul>
+                <ul className="header__menu__buttons">
+                    <li><button className="btn-login">Login</button></li>
+                    <li><button className="btn">Sign Up</button></li>
+                </ul>
+            </nav>
             <FaBars className="header__bars" />
         </header>
     )
