92d23feb5ae2a6e9ee0d009cde9b3de0735f840c
igormath <igorxt9@gmail.com>
2022-10-24 12:18:17 -0300

    :lipstick: Update Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,41 @@
+import { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
+import HiddenMenu from "../HiddenMenu/HiddenMenu";
 
 import './Header.css'
 
 const Header = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    const handleButton = () => {
+        setToggleMenu(current => !current);
+    };
+
+    useEffect(() => {
+        console.log(toggleMenu);
+    }, [toggleMenu]);
+
     return (
-        <header>
-            <h1 className="header__title">Shortly</h1>
-            <nav className="header__menu">
-                <ul className="header__menu__options">
-                    <li><a href="">Features</a></li>
-                    <li><a href="">Pricing</a></li>
-                    <li><a href="">Resources</a></li>
-                </ul>
-                <ul className="header__menu__buttons">
-                    <li><button className="btn-login">Login</button></li>
-                    <li><button className="btn">Sign Up</button></li>
-                </ul>
-            </nav>
-            <FaBars className="header__bars" />
-        </header>
-    )
-}
+        <>
+            <header>
+                <h1 className="header__title">Shortly</h1>
+                <nav className="header__menu">
+                    <ul className="header__menu__options">
+                        <li><a>Features</a></li>
+                        <li><a>Pricing</a></li>
+                        <li><a>Resources</a></li>
+                    </ul>
+                    <ul className="header__menu__buttons">
+                        <li><button className="btn-login">Login</button></li>
+                        <li><button className="btn">Sign Up</button></li>
+                    </ul>
+                </nav>
+                <FaBars className="header__bars" onClick={handleButton} />
+            </header>
+
+            <HiddenMenu hideToggle={toggleMenu} />
+        </>
+    );
+};
 
 export default Header;
