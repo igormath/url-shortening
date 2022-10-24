116259435cb72ce946780e5e167f1c36569dcc33
igormath <igorxt9@gmail.com>
2022-10-24 12:04:02 -0300

    :lipstick: Add StartSection component

---
+++ src/components/StartSection/StartSection.js
@@ -0,0 +1,12 @@
+import './StartSection.css';
+
+const StartSection = () => {
+    return (
+        <section className="start">
+            <h3 className="start__title">Boost your links today</h3>
+            <button className="start__btn btn">Get Started</button>
+        </section>
+    );
+};
+
+export default StartSection;
