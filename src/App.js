7f726ca482ce773929e2d1ef568d4bead45ad52c
igormath <igorxt9@gmail.com>
2022-10-24 12:08:59 -0300

    :bricks: Update App.js with new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import Header from './components/Header/Header';
+import PresentationSection from './components/PresentationSection/PresentationSection';
+import LinksSection from './components/LinksSection/LinksSection';
+import StatisticsSection from './components/StatisticsSection/StatisticsSection';
+import StartSection from './components/StartSection/StartSection';
 
 import './styles/generics/reset.css';
 import './styles/elements/base.css';
-import './styles/settings/colors.css'
+import './styles/settings/colors.css';
+import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
-    </div>
+      <main>
+        <PresentationSection />
+        <LinksSection />
+        <StatisticsSection />
+        <StartSection />
+        <Footer />
+      </main>
+    </>
   );
 }
 
